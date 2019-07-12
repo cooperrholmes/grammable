5766cc26824f5bedb43100c130a2f5eac92cc051
cooperrholmes@gmail.com <cooperrholmes@gmail.com>
2019-07-12 20:59:47 +0000

    Added validation for image uploads

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -147,15 +147,23 @@ RSpec.describe GramsController, type: :controller do
   end
 
   describe "grams#create action" do
+
     it "should require users to be logged in" do
       post :create, params: { gram: { message: "Hello" } }
       expect(response).to redirect_to new_user_session_path
     end
+
     it "should successfully create a new gram in our database" do
       user = FactoryBot.create(:user)
       sign_in user
 
-      post :create, params: { gram: {message: 'Hello!' } }
+      post :create, params: {
+        gram: {
+          message: 'Hello!',
+          picture: fixture_file_upload("/picture.png", 'image/png')
+        }
+      }
+      
       expect(response).to redirect_to root_path
 
       gram = Gram.last
