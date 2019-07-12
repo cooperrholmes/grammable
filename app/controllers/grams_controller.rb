993d6a304bad897c6ad0fe4cfb70976b90b95cde
cooperrholmes@gmail.com <cooperrholmes@gmail.com>
2019-07-12 19:57:59 +0000

    Added carrierwave for picture uploading, AWS for file storage

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -5,7 +5,7 @@ class GramsController < ApplicationController
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
     return render_not_found(:forbidden) if @gram.user != current_user
-    
+
     @gram.destroy
     redirect_to root_path
   end
@@ -28,6 +28,7 @@ class GramsController < ApplicationController
   end
 
   def index
+    @grams = Gram.all
   end
 
   def show
@@ -54,7 +55,7 @@ class GramsController < ApplicationController
   private
 
   def gram_params
-    params.require(:gram).permit(:message)
+    params.require(:gram).permit(:message, :picture)
   end
 
   def render_not_found(status=:not_found)
